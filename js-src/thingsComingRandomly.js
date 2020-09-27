4debb9218bfc00b320775861b546c487988dfdd6
Diego Bueno <diego@gwaya.com>
2020-09-27 20:38:16 +1000

    added new animals to the game

--- js-src/thingsComingRandomly.js
+++ js-src/thingsComingRandomly.js
@@ -1,8 +1,10 @@
-var rand = Math.random() * 4;
+var rand = Math.random() * 5;
 if (rand > 0 && rand <= 1) {
   gwd.actions.timeline.gotoAndPlay("projecttile_path_div", "farmer_start_moving");
 } else if (rand > 1 && rand <= 3) {
   gwd.actions.timeline.gotoAndPlay("projecttile_path_div", "onca_start_moving");
+} else if (rand > 3 && rand <= 4) {
+  gwd.actions.timeline.gotoAndPlay("projecttile_path_div", "sucuri_start");
 } else {
   gwd.actions.timeline.gotoAndPlay("projecttile_path_div", "doingNothing");
 }
