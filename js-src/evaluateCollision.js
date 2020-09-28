02f8714e51cd9e2d92e85db3f83c33fcd92f1563
Diego Bueno <diego@gwaya.com>
2020-09-28 15:02:16 +1000

    added Game Over screen

--- js-src/evaluateCollision.js
+++ js-src/evaluateCollision.js
@@ -11,25 +11,32 @@ var xDistanceFromFarmer = pathWidth - (xPositionOftheFarmer * (-1));
 var xDistanceFromOnca   = pathWidth - (xPositionOftheOnca * (-1));
 var xDistanceFromSucuri = (xPositionOftheSucuri/20)-35;
 
-document.getElementById("messages").textContent = " "  
-+ " saci_perere_divTop = " + saciTop 
+var messageToUser = ""
+/*
+messageToUser = " saci_perere_divTop = " + saciTop 
 + " xDistanceFromFarmer = " + xDistanceFromFarmer 
 + " xDistanceFromOnca: " + xDistanceFromOnca 
 + " xDistanceFromSucuri: " + xDistanceFromSucuri;
+*/
 
 var meetFarmer = (xDistanceFromFarmer >= 0 && xDistanceFromFarmer <= SAFE_DISTANCE);
 var meetOnca   = (xDistanceFromOnca >= 0 && xDistanceFromOnca <= SAFE_DISTANCE);
 var meetSucuri = (xDistanceFromSucuri >= 95 && xDistanceFromSucuri <= SAFE_DISTANCE);
 var saciIsSafe = (saciTop >= SAFE_DISTANCE);
+var saciIsSafeFromSucuri = (saciTop >= (SAFE_DISTANCE - 50));
 
 if (meetFarmer || meetOnca || meetSucuri ) {
 
-    if (saciIsSafe) {
-      addScore(1);
+    if ( ((meetFarmer || meetOnca ) && saciIsSafe ) ||
+         (meetSucuri && saciIsSafeFromSucuri) )
+    {
+      gwd.globalVars.score++;
+      addScoreToBar(gwd.globalVars.score);
     }
-    else if (meetFarmer && ( gwd.glovalVars.saciWillyWilly ) ) {
+    else if (meetFarmer && ( gwd.globalVars.saciWillyWilly ) ) {
       gwd.actions.timeline.gotoAndPlay("farmer-div", "farmer_falling_start");
-      addScore(10);
+      gwd.globalVars.score += 10;
+      addScoreToBar(gwd.globalVars.score);
     }
     else {
       gwd.actions.timeline.gotoAndPlay("saci_perere_div", "collision-start");
@@ -48,13 +55,53 @@ const {
 return(x);
 }
 
-function addScore(scoreGained) {
+function addScoreToBar(scoreGained) {
   var max_score = document.getElementById("max_score").offsetWidth;
   var gained_score = document.getElementById("gained_score").offsetWidth;
   var widthCurrent = document.getElementById("gained_score").style.width;
   
-  widthCurrent = Math.trunc((gained_score / max_score) * 100);
-  if ((widthCurrent + 1) < 68 ) { // 68 is the max % of this div
-    document.getElementById("gained_score").style.width =  (widthCurrent + scoreGained) + "%";
+  switch ( true )  {
+    case ( gwd.globalVars.score > 55 ):
+      messageToUser = "Keep focused!";
+    case ( gwd.globalVars.score > 50 && gwd.globalVars.score < 55):
+        messageToUser = "Excellent! You are close to Amazon river :)";
+        break;
+    case ( gwd.globalVars.score > 30 && gwd.globalVars.score < 35):
+        messageToUser = "Good mate!";
+        break;
+    case ( gwd.globalVars.score > 15 && gwd.globalVars.score < 20):
+        messageToUser = "Be careful with Sucuri! Jump when need";
+        break;  
+    case ( gwd.globalVars.score > 10 && gwd.globalVars.score < 15):
+         messageToUser = "Remember! 'Willy-Willy' do NOT save you from an Onça-pinatada";
+        break;  
+    case ( gwd.globalVars.score > 5  && gwd.globalVars.score < 10 ) :
+        messageToUser = 'Great! Use "Willy-Willy" against the destroyers to get plus 10 points!';
+        break;
+    case ( gwd.globalVars.score > 3 ):
+        messageToUser = "Good job! Keep going";
+        break;                          
+    default: {
+      messageToUser = "";
+    }
   }
+
+    document.getElementById("messages").textContent = messageToUser;
+  sleep(3000).then(() => {
+    messageToUser=""; document.getElementById("messages").textContent = messageToUser; ; 
+  });
+
+  //widthCurrent = Math.trunc((gained_score / max_score) * 100);
+  if ((gwd.globalVars.score) < 68 ) { // 68 is the max % of this div
+    document.getElementById("gained_score").style.width =  gwd.globalVars.score + "%";
+  }
+  else {
+    document.getElementById("gained_score").style.width =  "68%";
+  }
+  document.getElementById("score").textContent = gwd.globalVars.score;
+}
+
+// sleep time expects milliseconds
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
