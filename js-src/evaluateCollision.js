94794eefbe004e782be5fb4c8594ab3d064c8dd0
Diego Bueno <diego@gwaya.com>
2020-09-28 11:04:23 +1000

    set up score and game over in case collision with dangerous animals

--- js-src/evaluateCollision.js
+++ js-src/evaluateCollision.js
@@ -1,34 +1,60 @@
+const SAFE_DISTANCE = 100;
 var pathWidth = document.getElementById("game_main_div").offsetWidth;
+var saciTop = document.getElementById("saci_onelegged_walking_2").offsetTop * (-1);
 var saciWidth = document.getElementById("saci_perere_div").offsetWidth;
-var max_score = document.getElementById("max_score").offsetWidth;
-var gained_score = document.getElementById("gained_score").offsetWidth;
-var widthCurrent = document.getElementById("gained_score").style.width;
-var farmer = document.getElementById("farmer-div");
-const {
-  x, y, z
-} = getTranslateValues(farmer);
-var xPositionOftheFarmer = x;
-var distance = pathWidth - (xPositionOftheFarmer * (-1));
-document.getElementById("messages").textContent = "pathWidth = " + pathWidth 
-+ ", saciWidth = " + saciWidth 
-+ ", Farmer x: " + xPositionOftheFarmer 
-+ " distance: " + distance 
-+ " max_score: " + max_score 
-+ " gained_score: " + gained_score
-+ " widthCurrent: " + widthCurrent;
 
-if (distance >= 0 && distance <= 100) {
-  if (!gwd.glovalVars.saciWillyWilly) {
-    gwd.actions.timeline.gotoAndPlay("saci_perere_div", "collision-start");
-  } else { 
-    widthCurrent = Math.trunc((gained_score / max_score) * 100);
-    if ((widthCurrent + 1) < 68 ) { // 68 is the max % of this div
-      document.getElementById("gained_score").style.width =  (widthCurrent + 1) + "%";
+var xPositionOftheFarmer = getXaxisPosition(document.getElementById("farmer-div"));
+var xPositionOftheOnca   = getXaxisPosition(document.getElementById("onca-div"));
+var xPositionOftheSucuri = document.getElementById("sucuri-div").offsetLeft;
+
+var xDistanceFromFarmer = pathWidth - (xPositionOftheFarmer * (-1));
+var xDistanceFromOnca   = pathWidth - (xPositionOftheOnca * (-1));
+var xDistanceFromSucuri = (xPositionOftheSucuri/20)-35;
+
+document.getElementById("messages").textContent = " "  
++ " saci_perere_divTop = " + saciTop 
++ " xDistanceFromFarmer = " + xDistanceFromFarmer 
++ " xDistanceFromOnca: " + xDistanceFromOnca 
++ " xDistanceFromSucuri: " + xDistanceFromSucuri;
+
+var meetFarmer = (xDistanceFromFarmer >= 0 && xDistanceFromFarmer <= SAFE_DISTANCE);
+var meetOnca   = (xDistanceFromOnca >= 0 && xDistanceFromOnca <= SAFE_DISTANCE);
+var meetSucuri = (xDistanceFromSucuri >= 95 && xDistanceFromSucuri <= SAFE_DISTANCE);
+var saciIsSafe = (saciTop >= SAFE_DISTANCE);
+
+if (meetFarmer || meetOnca || meetSucuri ) {
+
+    if (saciIsSafe) {
+      addScore(1);
+    }
+    else if (meetFarmer && ( gwd.glovalVars.saciWillyWilly ) ) {
+      gwd.actions.timeline.gotoAndPlay("farmer-div", "farmer_falling_start");
+      addScore(10);
     }
     else {
-      alert("Congratulations! You win the game. Keep saving the Amazon!");  
-    }
-    gwd.actions.timeline.gotoAndPlay("farmer-div", "farmer_falling_start");
+      gwd.actions.timeline.gotoAndPlay("saci_perere_div", "collision-start");
   }
+
+  if (document.getElementById("gained_score").style.width >= 68) {
+    alert("Congratulations! You win the game. Keep saving the Amazon!");  
+  }
+
 }
 
+function getXaxisPosition(objPositions) {
+const {
+  x, y, z
+} = getTranslateValues(objPositions);
+return(x);
+}
+
+function addScore(scoreGained) {
+  var max_score = document.getElementById("max_score").offsetWidth;
+  var gained_score = document.getElementById("gained_score").offsetWidth;
+  var widthCurrent = document.getElementById("gained_score").style.width;
+  
+  widthCurrent = Math.trunc((gained_score / max_score) * 100);
+  if ((widthCurrent + 1) < 68 ) { // 68 is the max % of this div
+    document.getElementById("gained_score").style.width =  (widthCurrent + scoreGained) + "%";
+  }
+}
